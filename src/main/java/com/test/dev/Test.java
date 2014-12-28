c49399c9453bbe5148f474b31343335f2f62402f
vankadaruhari <vankadaruhari@gmail.com>
2014-12-28 06:29:57 +0530

    This is a test Project

---
+++ src/main/java/com/test/dev/Test.java
@@ -0,0 +1,14 @@
+package com.test.dev;
+
+import java.util.Date;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("latest Date is..."+new Date());
+
+	}
+}
